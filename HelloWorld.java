ed6b212aca1f5ecf20aad07a172d1ea6f4eaac31
vicky1408 <vigneshv01@gmail.com>
2019-09-07 11:17:49 +0530

    incorrect syntax committed

--- HelloWorld.java
+++ HelloWorld.java
@@ -33,7 +33,7 @@
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
 public class HelloWorld {
-  public static void main(String[] args) {
+  public static void main(String[] arg {
     System.out.println("Hello Vaishu, Pavishka");
   }
 }
